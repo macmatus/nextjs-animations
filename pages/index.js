ca7fa110b32aa80fe60670961699a8ebd6be5f98
Maciej Matusiak <maciekmatusiak@hotmail.com>
2022-05-05 23:14:42 +0100

    setup fetch for homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
+import { useState, useEffect } from 'react';
+
 export default function Home() {
+  const url = 'https://my-json-server.typicode.com/macmatus/nextjs-animations/bikes';
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+      })
+  }, [])
+
   return (
-    <h1>Hello world</h1>
+    <div>
+      {data?.map((bike, key) => {
+        return (
+          <div key={key}>
+            <img src={`http://localhost:3000/pages/images/${bike.images[0]}`} alt="" />
+            <h2>{bike.model}</h2>
+            <p>£{bike.price}</p>
+          </div>
+        )
+      })}
+    </div>
   )
 }
